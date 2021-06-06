be53d70328d36f6784fe68e47976c7fbf88cf3d4
solankisahil28102001 <85236142+solankisahil28102001@users.noreply.github.com>
2021-06-06 12:32:45 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1><u>Javascript and React.js</u></h1>
+      <p>
+        This bootcamp is very good to learn basics of web development, Thanks to <b>ShapeAI</b> to provide this amazing bootcamp.
+      </p>
+    </div>
+  );
+}
+export default Note;
